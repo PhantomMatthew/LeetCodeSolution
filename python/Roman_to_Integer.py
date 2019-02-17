b4f5157750b1118e3565271485b19868b7e74787
Jiabei Yuan <jiabei.yuan@gmail.com>
2019-02-17 20:16:18 +0800

    update romain_to_integer

--- python/Roman_to_Integer.py
+++ python/Roman_to_Integer.py
@@ -10,15 +10,18 @@ class Solution:
         #             sum -= d[s[i]]
         #         sum += d[s[]]
         # return sum
-        
+
         i = 0
         while i < len(s) - 1:
-            if s[i] + s[i+1] in d.values:
+            if s[i] + s[i+1] in d.values():
                 sum += d[s[i] + s[i+1]]
                 i += 2
             else:
                 sum += d[s[i]]
                 i += 1
+                
+        if i != len(s):
+            sum += d[s[len(s) - 1]]
         return sum
 
 solution = Solution()
