723c5eb5131972dd55e99a31273b19c18f758b29
Jiabei Yuan <jiabei.yuan@gmail.com>
2019-02-17 18:44:32 +0800

    add palindrome and romain_to_integer

---
+++ python/Roman_to_Integer.py
@@ -0,0 +1,8 @@
+class Solution:
+    def romanToInt(self, s: 'str') -> 'int':
+        #keys = {}
+        for i in range(len(s)):
+            print(i)
+
+solution = Solution()
+solution.romanToInt("CM")
