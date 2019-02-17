b53674f817d95176c925a1983583601cd6553923
Jiabei Yuan <jiabei.yuan@gmail.com>
2019-02-17 19:03:06 +0800

    add roman_to_integer

--- python/Roman_to_Integer.py
+++ python/Roman_to_Integer.py
@@ -1,8 +1,25 @@
 class Solution:
     def romanToInt(self, s: 'str') -> 'int':
-        #keys = {}
-        for i in range(len(s)):
-            print(i)
+        d = {'I': 1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+        sum = 0
+        # for i in range(len(s)):
+        #     if i < range(len(s)):
+        #         if d[s[i]] <= d[s[i+1]]:
+        #             sum += d[s[i]]
+        #         else:
+        #             sum -= d[s[i]]
+        #         sum += d[s[]]
+        # return sum
+        
+        i = 0
+        while i < len(s) - 1:
+            if s[i] + s[i+1] in d.values:
+                sum += d[s[i] + s[i+1]]
+                i += 2
+            else:
+                sum += d[s[i]]
+                i += 1
+        return sum
 
 solution = Solution()
 solution.romanToInt("CM")
