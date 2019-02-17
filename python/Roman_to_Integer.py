67b0d60c217d6ae75ac64a29c4673d3a0ba3896c
phantommatthew <jiabei.yuan@gmail.com>
2019-02-17 21:57:04 +0800

    update roman_to_integer for correct answer

--- python/Roman_to_Integer.py
+++ python/Roman_to_Integer.py
@@ -3,17 +3,9 @@ class Solution:
         d1 = {'I': 1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
         d2 = {'IV':4, 'IX': 9, 'XL':40, 'XC':90, 'CD':400, 'CM': 900}
         sum = 0
-        # for i in range(len(s)):
-        #     if i < range(len(s)):
-        #         if d[s[i]] <= d[s[i+1]]:
-        #             sum += d[s[i]]
-        #         else:
-        #             sum -= d[s[i]]
-        #         sum += d[s[]]
-        # return sum
         i = 0
         while i < len(s) - 1:
-            if s[i] + s[i+1] in d2.values():
+            if s[i] + s[i+1] in d2.keys():
                 sum += d2[s[i] + s[i+1]]
                 i += 2
             else:
@@ -25,4 +17,4 @@ class Solution:
         return sum
 
 solution = Solution()
-solution.romanToInt("CM")
+print(solution.romanToInt("CM"))
