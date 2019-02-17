8435893badcd362629668f607f226431f79ffee9
Jiabei Yuan <jiabei.yuan@gmail.com>
2019-02-17 21:37:31 +0800

    update romain_to_integer

--- python/Roman_to_Integer.py
+++ python/Roman_to_Integer.py
@@ -1,6 +1,7 @@
 class Solution:
     def romanToInt(self, s: 'str') -> 'int':
-        d = {'I': 1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+        d1 = {'I': 1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+        d2 = {'IV':4, 'IX': 9, 'XL':40, 'XC':90, 'CD':400, 'CM': 900}
         sum = 0
         # for i in range(len(s)):
         #     if i < range(len(s)):
@@ -10,18 +11,17 @@ class Solution:
         #             sum -= d[s[i]]
         #         sum += d[s[]]
         # return sum
-
         i = 0
         while i < len(s) - 1:
-            if s[i] + s[i+1] in d.values():
-                sum += d[s[i] + s[i+1]]
+            if s[i] + s[i+1] in d2.values():
+                sum += d2[s[i] + s[i+1]]
                 i += 2
             else:
-                sum += d[s[i]]
+                sum += d1[s[i]]
                 i += 1
                 
         if i != len(s):
-            sum += d[s[len(s) - 1]]
+            sum += d1[s[len(s) - 1]]
         return sum
 
 solution = Solution()
